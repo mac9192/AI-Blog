670b5a0657fc039808a9795c5a88980c350a237d
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-11-14 16:18:19 -0500

    Update PI Page

--- app/[lang]/personal-injury/page.tsx
+++ app/[lang]/personal-injury/page.tsx
@@ -16,6 +16,7 @@ import lawfam from '../../../public/assets/lawfam.png'
 import bgpi from '../../../public/assets/carcrashpi.jpeg'
 import injury from '../../../public/assets/icon-injury.svg'
 import HomeBlog from '@frontComponents/HomeBlog'
+import carAccident from '../../../public/assets/card-car-accident.jpeg'
  
 
 
@@ -126,13 +127,44 @@ const PersonalInjury = async ({
         
 </div>
 
-<div className="bg-white h-screen">
-
+<div className="mt-[400px] md:container md:mx-auto md:pl-[130px] md:pr-[130px]">
+      <p className="mt-[400px] text-4xl tracking-wide w-full border-b-4 border-dotted border-logoBlue text-logoBlue p-3">Personal Injury Services:</p>
 </div>
-
+ 
+<div className='grid md:container md:mx-auto md:grid-cols-3 sm:grid-cols-1 gap-x-2 gap-y-3 grid-flow-row-dense pt-10 max-w-[1000px] md:pl-[130px] md:pr-[130px]  '>
+   
+      <div className='bg-logoBlue   shadow-xl min-h-[350px] row-span-1 md:col-span-2' > 
+      <div className="flex md:flex-row w-full pt-5 sm:flex-col sm:pl-10 ">
+            <div className="flex flex-row md:w-7/8 sm:w-full sm:pr-8">
+            <Image className=" p-6 pt-8 pr-3 sm:pl-1" width={480} height={2000} src={carAccident} alt=""/>
+            </div>
+            <div className="flex flex-col pr-10"> 
+                <p className="md:text-[28px] sm:text-2xl text-white  pt-7 p-2 border-dotted border-b-4 border-white w-full">MOTOR VEHICLE ACCIDENTS</p>
+            
+                <p className="pt-5 text-2xl text-white">FROM BASIC RESEARCH TO PRACTICAL APPLICATIONS</p>
+                <p className="w-full text-1xl pt-6 text-white">We provide our clients with compassionate, yet aggressive representatio  vehicle accidents.</p>
+                <p className="w-full text-1xl  text-white pt-12 sm:mb-5">Learn More</p>
+            </div>
+        </div>
+      </div>
+      <div className=' bg-logoBlue   shadow-xl min-h-[350px]' />
+ 
+ 
+   
+    </div>
+     
+<div className='grid md:grid-cols-3 md:container md:mx-auto sm:grid-cols-1 gap-x-2 gap-y-3 grid-flow-row-dense  mt-[19px] md:pl-[130px] md:pr-[130px] '>
+       
+      <div className='bg-logoBlue   shadow-xl min-h-[350px]' />
+      <div className='bg-logoBlue    shadow-xl min-h-[350px]' />
+      <div className='bg-logoBlue    shadow-xl min-h-[350px]' />
+ 
+ 
+   
+    </div>
  
 
-<div className="md:bg-white sm:bg-logoBlue md:p-0 md:mt-[338px] lg:mt-[315px] sm:mt-[0px] text-center   md:mx-auto sm:text-white md:text-black sm:p-3  md:mb-[-40px] pt-[200px]  ">
+<div className="md:bg-white sm:bg-logoBlue md:p-0 md:mt-[338px] lg:mt-[25px] sm:mt-[0px] text-center   md:mx-auto sm:text-white md:text-black sm:p-3  md:mb-[-40px] pt-[200px]  ">
     <div className="flex sm:flex-col md:flex-row md:container md:mx-auto    text-center mx-auto pt-16  mb-16">
             
             <div className="md:w-/4 sm:text-center sm:mx-auto sm:w-full md:text-[22px] md:max-w-[800px] md:text-right">
